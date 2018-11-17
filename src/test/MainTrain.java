d4268d9d935998be50a237adce4e9389e4b097e9
Inbal <inbalmiezels@gmail.com>
2018-11-17 15:56:48 +0200

    Added private tests to the project

---
+++ src/test/MainTrain.java
@@ -0,0 +1,35 @@
+package test;
+
+//import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MainTrain {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        try {
+            ExecutorService exec = Executors.newFixedThreadPool(5);
+            exec.execute(() -> {
+                TestServer.runClient(5555, "s|g");
+            });
+            Thread.sleep(100);
+            exec.execute(() -> {
+                TestServer.runClient(5555, "s||g");
+            });
+            exec.execute(() -> {
+                TestServer.runClient(5555, "s|g");
+            });
+
+            exec.execute(() -> {
+                TestServer.runClient(5555, "s|||g");
+            });
+            exec.execute(() -> {
+                TestServer.runClient(5555, "s||g");
+            });
+
+            //TestServer.runClient(5555);
+        } finally {
+        }
+    }
+}
