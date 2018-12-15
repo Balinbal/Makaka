403631684ec40355b1e3fd6a475cbc8bc839eec4
Inbal <inbalmiezels@gmail.com>
2018-12-15 15:07:15 +0200

    Get level API and ORM working with Tomcat

--- Server/src/main/java/MainServer.java
+++ Server/src/main/java/MainServer.java
@@ -1,10 +1,32 @@
 import ClientHandler.MyCHandler;
+import Model.Level;
+import Model.ModelUtil;
 import Server.MyServer;
 import Server.Server;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
 public class MainServer {
 
     public static void main(String[] args) {
+
+//        SessionFactory sessionFactory = ModelUtil.getSessionFactory();
+//        Session session = sessionFactory.openSession();
+//        session.beginTransaction();
+//
+//        Level dbLevel = new Level();
+//        dbLevel.setBoard("SXG");
+//        dbLevel.setLevel(1);
+//
+//        Level dbLevel2 = new Level();
+//        dbLevel2.setBoard("SXXG");
+//        dbLevel2.setLevel(2);
+//
+//        session.save(dbLevel);
+//        session.save(dbLevel2);
+//        session.getTransaction().commit();
+//        session.close();
+
 
         Server s =  new MyServer(5555);
         s.start(new MyCHandler());
